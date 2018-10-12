96767aa775e168e5ceb1217213537b27826bb5aa
Clive Galway <evilc@evilc.com>
2018-10-13 00:12:00 +0100

    Add missing files

---
+++ DTOs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMouse_HidLibrary
+{
+    public enum BindingType { Axis, Button }
+
+    public struct SpaceMouseUpdate
+    {
+        public BindingType BindingType { get; set; }
+        public int Index { get; set; }
+        public int Value { get; set; }
+    }
+}
